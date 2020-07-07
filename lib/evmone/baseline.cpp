674c974c4f9515ae47e7e97836065dcd69eed9a5
Paweł Bylica <chfast@gmail.com>
2020-07-07 22:15:36 +0200

    baseline: Add loop and check instruction requirements

--- lib/evmone/baseline.cpp
+++ lib/evmone/baseline.cpp
@@ -3,21 +3,52 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #include "baseline.hpp"
+#include "analysis.hpp"
+#include <memory>
 
 namespace evmone
 {
-evmc_result baseline_execute(evmc_vm* vm, const evmc_host_interface* host, evmc_host_context* ctx,
-    evmc_revision rev, const evmc_message* msg, const uint8_t* code, size_t code_size) noexcept
+evmc_result baseline_execute([[maybe_unused]] evmc_vm* vm, const evmc_host_interface* host,
+    evmc_host_context* ctx, evmc_revision rev, const evmc_message* msg, const uint8_t* code,
+    size_t code_size) noexcept
 {
-    (void)vm;
-    (void)host;
-    (void)ctx;
-    (void)rev;
-    (void)msg;
-    (void)code;
-    (void)code_size;
+    const auto op_tbl = get_op_table(rev);
+
+    auto state = std::make_unique<ExecutionState>(*msg, rev, *host, ctx, code, code_size);
+
     evmc_result result{};
-    result.status_code = EVMC_INTERNAL_ERROR;
+
+    for (size_t pc = 0; pc != code_size; ++pc)
+    {
+        const auto op = code[pc];
+        const auto metrics = op_tbl[op];
+
+        if ((state->gas_left -= metrics.gas_cost) < 0)
+        {
+            result.status_code = EVMC_OUT_OF_GAS;
+            break;
+        }
+
+        if (state->stack.size() < metrics.stack_req)
+        {
+            result.status_code = EVMC_STACK_UNDERFLOW;
+            break;
+        }
+
+        if (state->stack.size() + metrics.stack_change > evm_stack::limit)
+        {
+            result.status_code = EVMC_STACK_OVERFLOW;
+            break;
+        }
+
+        switch (op)
+        {
+        case OP_STOP:
+            goto exit;
+        }
+    }
+
+exit:
     return result;
 }
 }  // namespace evmone
