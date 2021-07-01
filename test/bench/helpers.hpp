d9258e3a76ba68415e0f2838eb02a6035b980661
Andrei Maiboroda <andrei@ethereum.org>
2021-07-01 15:33:57 +0200

    Support EOF execution in advanced

--- test/bench/helpers.hpp
+++ test/bench/helpers.hpp
@@ -38,7 +38,7 @@ struct FakeCodeAnalysis
 inline AdvancedCodeAnalysis advanced_analyse(evmc_revision rev, bytes_view code)
 {
     // TODO: Change analyze() signature.
-    return analyze(rev, code.data(), code.size());
+    return analyze(rev, code.data(), 0, code.size());
 }
 
 inline baseline::CodeAnalysis baseline_analyse(evmc_revision rev, bytes_view code)
