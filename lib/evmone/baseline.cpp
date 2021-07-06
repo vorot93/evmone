d9d8fd5210ddcec7776ec51a4a95ec3339e0177f
Andrei Maiboroda <andrei@ethereum.org>
2021-07-06 18:58:36 +0200

    Implement RJUMPTABLE in Baseline

--- lib/evmone/baseline.cpp
+++ lib/evmone/baseline.cpp
@@ -45,7 +45,7 @@ CodeAnalysis analyze_jumpdests(
     // TODO: Using fixed-size padding of 33, the padded code buffer and jumpdest bitmap can be
     //       created with single allocation.
 
-    return CodeAnalysis{std::move(padded_code), std::move(map), code_begin, code_end};
+    return CodeAnalysis{std::move(padded_code), std::move(map), code_begin, code_end, {}};
 }
 
 
@@ -72,9 +72,29 @@ CodeAnalysis analyze_eof2(const uint8_t* code, size_t /*code_size*/, const EOF2H
     // Set final STOP/INVALID at the code end.
     std::fill_n(padded_code.get() + code_end, code_padding, uint8_t{OP_INVALID});
 
-    // Skipping jumpdest analysis
+    // Read tables
+    CodeAnalysis::TableList tables;
+    tables.reserve(header.table_sizes.size());
 
-    return CodeAnalysis{std::move(padded_code), {}, code_begin, code_end};
+    const auto* table_section = code + header.tables_begin();
+    for (const auto table_size : header.table_sizes)
+    {
+        std::vector<int16_t> table;
+        table.reserve(static_cast<size_t>(table_size));
+        const auto* table_section_end = table_section + 2 * table_size;
+        while (table_section != table_section_end)
+        {
+            const auto offset_hi = *(table_section);
+            const auto offset_lo = *(table_section + 1);
+            const int16_t offset = static_cast<int16_t>((offset_hi << 8) + offset_lo);
+            table.push_back(offset);
+
+            table_section += 2;
+        }
+        tables.emplace_back(std::move(table));
+    }
+
+    return CodeAnalysis{std::move(padded_code), {}, code_begin, code_end, std::move(tables)};
 }
 }  // namespace
 
@@ -117,6 +137,26 @@ const uint8_t* op_rjump(const uint8_t* pc) noexcept
     const auto offset_lo = *(pc + 2);
     const auto offset = static_cast<int16_t>((offset_hi << 8) + offset_lo);
     return pc + 3 + offset;  // PC_post_rjump + offset
+}
+
+const uint8_t* op_rjumptable(
+    ExecutionState& state, const CodeAnalysis::TableList& tables, const uint8_t* pc) noexcept
+{
+    // Reading next 2 bytes is guaranteed to be safe by deploy-time validation.
+    const auto table_index_hi = *(pc + 1);
+    const auto table_index_lo = *(pc + 2);
+    const auto table_index = static_cast<uint16_t>((table_index_hi << 8) + table_index_lo);
+
+    // table_index is guaranteed to be within tables bounds by deploy-time validation.
+
+    const auto index = state.stack.pop();
+    if (index >= tables[table_index].size())
+    {
+        state.status = EVMC_BAD_JUMP_DESTINATION;  // TODO new error code
+        return pc;                                 // This value is ignored.
+    }
+
+    return pc + 3 + tables[table_index][static_cast<size_t>(index)];  // PC_post_rjumptable + offset
 }
 
 template <size_t Len>
@@ -506,6 +546,11 @@ evmc_result execute(const VM& vm, ExecutionState& state, const CodeAnalysis& ana
                 pc += 2;
             }
             break;
+        case OP_RJUMPTABLE:
+            pc = op_rjumptable(state, analysis.tables, pc);
+            if (state.status == EVMC_BAD_JUMP_DESTINATION)
+                goto exit;
+            continue;
 
         case OP_PUSH1:
             pc = load_push<1>(state, pc + 1);
